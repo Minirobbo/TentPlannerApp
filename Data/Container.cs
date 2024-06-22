59a96134065abe587c209c85309f7348a45d08b9
Isaac Robinson <isaac@irobinson.org>
2024-06-23 00:45:43 +1000

    Add project files.

---
+++ Data/Container.cs
@@ -0,0 +1,17 @@
+﻿namespace TentPlannerApp.Data
+{
+    public class Container
+    {
+        public string Id { get; init; }
+        public string Name { get; set; }
+        public bool Expanded { get; set; } = true;
+
+        public Container(string baseName, int number)
+        {
+            Id = "Container " + number;
+            Name = baseName + " " + number;
+        }
+
+        public Container() {}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+using Blazored.LocalStorage;
+using MudBlazor.Services;
+using TentPlannerApp.Components;
+using TentPlannerApp.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
+
+builder.Services.AddScoped<SettingsService>();
+builder.Services.AddScoped<DataService>();
+
+builder.Services.AddBlazoredLocalStorage();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
