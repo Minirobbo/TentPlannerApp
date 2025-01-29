7dcf31a36f45f14bf49e80fadc236515ad18eae0
Isaac Robinson <isaac@irobinson.org>
2025-01-29 22:24:28 +1000

    Abstracted Tent planning to its own razor component.

---
+++ WASM_App/Data/OverlaysControl.cs
@@ -0,0 +1,17 @@
+﻿namespace WASM_App.Data
+{
+    public class OverlaysControl
+    {
+        public bool _settingsOpen { get; set; } = false;
+        public bool _uploadFileOpen { get; set; } = false;
+        public bool _editPersonOpen { get; set; } = false;
+        public bool _editingContainer { get; set; } = false;
+
+        public EventHandler OnRefresh { get; set; }
+
+        public void Refresh()
+        {
+            OnRefresh.Invoke(this, null);
+        }
+    }
+}
